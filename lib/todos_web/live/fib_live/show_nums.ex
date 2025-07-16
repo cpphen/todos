825c0eb2757414725630cd6e6b78a345ae505c8f
cpphen <btnysci@yahoo.com>
2025-07-16 10:11:15 -0700

    working input

--- lib/todos_web/live/fib_live/show_nums.ex
+++ lib/todos_web/live/fib_live/show_nums.ex
@@ -2,8 +2,11 @@ defmodule TodosWeb.FibLive.ShowNums do
   use TodosWeb, :live_view
   alias Todos.FibNums
 
+  # Use `assign` to set values to socket object
   @impl true
   def mount(_params, _session, socket) do
+    socket = socket
+      |> assign(form: %{"seq_num" => "Initial Value"})
     {:ok, socket}
   end
 
@@ -15,6 +18,13 @@ defmodule TodosWeb.FibLive.ShowNums do
   @impl true
   def handle_info(:refresh, socket) do
     {:noreply, assign(socket, :fibs, FibNums.get_fibs())}
+  end
+
+  @impl true
+  def handle_event("calculate", %{"seq_num" => seq_num_params}, socket) do
+    IO.puts "\n HERE \n"
+    IO.inspect(seq_num_params)
+    {:noreply, assign(socket, form: to_form(seq_num_params, as: :seq_num))}
   end
 
   defp apply_action(socket, :fibs, _params) do
